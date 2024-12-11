62cae752ba37672da61a72e9cc45ac4d443ad7f3
Gabriel Duarte Marques <126476225+GabrielRainwalker@users.noreply.github.com>
2024-12-10 21:42:36 -0300

    Delete collision.cpp

--- collision.cpp
+++
@@ -1,8 +0,0 @@
-#include "collision.h"
-
-bool checkCollision(const BoundingBox& a, const BoundingBox& b) {
-    return (a.x < b.x + b.width &&
-            a.x + a.width > b.x &&
-            a.y < b.y + b.height &&
-            a.y + a.height > b.y);
-}
